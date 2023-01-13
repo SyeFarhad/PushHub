9e204df338bb0c06b04f8149e5f3fcda88d5c683
sayedfarhad <sayedfarhad@sayeds-mbp-2.hsd1.ca.comcast.net>
2023-01-12 17:08:00 -0800

    This is our Class Practice!

---
+++ src/PushHub/PushPractice.java
@@ -0,0 +1,26 @@
+package PushHub;
+
+public class PushPractice {
+
+	
+	public static void main(String[] args) {
+
+		getWhileLoopResult(1, 50);
+
+	}
+
+	public static void getWhileLoopResult(int minNum, int maxNum) {
+
+		int i = 1;
+		while (i <= 50) {
+
+			System.out.println(i);
+
+			i++;
+
+		}
+
+	}
+
+	
+}
